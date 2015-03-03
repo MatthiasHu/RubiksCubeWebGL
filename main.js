3f8deae5ee333b5c07005d2c901d0b7f78b8191e
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2015-03-03 04:11:42 +0100

    some setup, including shaders

---
+++ main.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// global variables:
+var canvas;
+var gl = null; // the webgl context
+
+
+function onLoad() {
+	var div = document.getElementById("randomdiv");
+	div.innerHTML = "sooooo much randomness";
+
+	canvas = document.getElementById("webgl canvas");
+
+	// try to initialize webgl
+	try {gl = canvas.getContext("webgl");} catch (e) {}
+	if (!gl) try {gl = canvas.getContext("experimental-webgl");} catch (e) {}
+	if (!gl) {
+		alert("Sorry, WebGL couldn't be initialized.");
+		return;
+	}
+
+	// initialize shaders
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertexShader, document.getElementById("vertex shader").innerHTML);
+	gl.compileShader(vertexShader);
+	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShader, document.getElementById("fragment shader").innerHTML);
+	gl.compileShader(fragmentShader);
+	var shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		alert("Sorry, shaders couldn't be compiled/linked.");
+		return null;
+	}
+	
+	
+	alert("ok");
+	
+}
