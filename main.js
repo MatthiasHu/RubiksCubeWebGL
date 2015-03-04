52a0f6b493719efcc2a5eb5b9c2324a41966af45
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2015-03-04 19:17:33 +0100

    added projection and modelview matrix; now using the glMatrix library

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 "use strict";
 
+// note: mat4.* (and vec3.*) are calls to the glMatrix library
+
 // global variables:
 var canvas;
 var gl = null; // the webgl context
 var vertexBuffer; // gl-buffer for vertices of one primitive
-var vertPosAttributeLocation; // used to identify the vertex position attribut
-	// in the vertex shader and to bind vertex array data to this attribute
+var shaderLocations = new Object(); // will hold junctures to the shaders
+shaderLocations.aVertexPosition = null; // vertex position attribute
+shaderLocations.uPMatrix = null; // projection matrix uniform
+shaderLocations.uMVMatrix = null; // model view matrix uniform
+var pMatrix; // the projection matrix
 
 
 function onLoad() {
 	var div = document.getElementById("randomdiv");
 	div.innerHTML = "sooooo much randomness";
+	var perspMat = mat4.create();
+	mat4.perspective(perspMat, Math.PI/4, 2, 0.01, 1000);
+	div.innerHTML = mat4.str(perspMat);
 
 	canvas = document.getElementById("webgl canvas");
 
@@ -41,14 +49,28 @@ function onLoad() {
 		return null;
 	}
 	gl.useProgram(shaderProgram);
-	vertPosAttributeLocation = gl.getAttribLocation(shaderProgram,
-		"aVertexPosition");
-	gl.enableVertexAttribArray(vertPosAttributeLocation);
-	
-	alert("ok");
-	initVertexBuffers();
-	render();
 
+	// get juctures to shaders
+	shaderLocations.aVertexPosition =
+		gl.getAttribLocation(shaderProgram, "aVertexPosition");
+	gl.enableVertexAttribArray(shaderLocations.aVertexPosition);
+	shaderLocations.uPMatrix =
+		gl.getUniformLocation(shaderProgram, "uPMatrix");
+	shaderLocations.uMVMatrix =
+		gl.getUniformLocation(shaderProgram, "uMVMatrix");
+	
+	
+	// initialize objects to be rendered
+	initVertexBuffers();
+
+	// setup projection matrix
+	pMatrix = mat4.create();
+	// 45 degrees vertical field of view, aspect ratio 1
+	mat4.perspective(pMatrix, Math.PI/4, 1, 0.01, 1000);
+	gl.uniformMatrix4fv(shaderLocations.uPMatrix, false, pMatrix);
+
+
+	render();
 }
 
 function initVertexBuffers() {
@@ -67,12 +89,34 @@ function initVertexBuffers() {
 
 function render() {
 	if (!gl) return;
+	// clear background
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-	gl.vertexAttribPointer(vertPosAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	// setup modelview matrix
+	var mvMatrix = mat4.create(); // identity matrix
+	mat4.identity(mvMatrix);
+
+	renderObject0(matTranslate(mvMatrix, 0, 0, -15));
 
 	alert("rendered.");
+}
+
+function renderObject0(mvMatrix) {
+	// pass modelview matrix
+	gl.uniformMatrix4fv(shaderLocations.uMVMatrix, false, mvMatrix);
+
+	// render content of vertexBuffer
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.vertexAttribPointer(shaderLocations.aVertexPosition,
+		3, gl.FLOAT, false, 0, 0);
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+
+// matrix helper functions
+function matTranslate(matrix, x, y, z) {
+	var result = mat4.create();
+	mat4.translate(result, matrix, vec3.fromValues(x, y, z));
+	return result;
 }
